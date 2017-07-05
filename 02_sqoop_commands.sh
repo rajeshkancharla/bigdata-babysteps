7c55f14ebdf515b41ac7fffc4d8ce3ee5919c3fa
Rajesh Kancharla <rajeshkancharla@users.noreply.github.com>
2017-07-05 10:57:28 +0800

    Update 02_sqoop_commands.sh

--- 02_sqoop_commands.sh
+++ 02_sqoop_commands.sh
@@ -1,3 +1,7 @@
+# About Sqoop:
+# Sqoop is a tool designed to transfer data between Hadoop and relational databases or mainframes. You can use Sqoop to import data from a relational database management system (RDBMS) such as MySQL or Oracle or a mainframe into the Hadoop Distributed File System (HDFS), transform the data in Hadoop MapReduce, and then export the data back into an RDBMS.
+# Sqoop automates most of this process, relying on the database to describe the schema for the data to be imported. Sqoop uses MapReduce to import and export the data, which provides parallel operation as well as fault tolerance.
+
 # List all the databases available 
 sqoop list-databases 
   --connect jdbc:mysql://<server_ip> 
