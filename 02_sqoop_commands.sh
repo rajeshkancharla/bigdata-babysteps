0ddb5205a328a5c5e91a02a81b1ecaea70b03da2
Rajesh Kancharla <rajeshkancharla@users.noreply.github.com>
2017-07-05 10:40:00 +0800

    Update 02_sqoop_commands.sh

--- 02_sqoop_commands.sh
+++ 02_sqoop_commands.sh
@@ -1,12 +1,11 @@
 # List all the databases available 
-
 sqoop list-databases 
   --connect jdbc:mysql://<server_ip> 
   --username <db_user_name> 
   --password <db_user_name_password>
 
-# List all tables available in a database. Here database name is rajeshk
 
+# List all tables available in a database. Here database name is rajeshk
 sqoop list-tables 
   --connect jdbc:mysql://<server_ip>/rajeshk 
   --driver com.mysql.jdbc.Driver 
@@ -15,16 +14,16 @@ sqoop list-tables
 
 
 # Import table from RDBMS to HDFS
-
+    # -m 1 represents that there is one mapper
+    # by default there are 4 mappers used when no mapper is specified
+    # this command creates file in the HDFS in the home directory for the user ~/emp/part-m-00000
+    # the m represents that this is a mapper job
 sqoop import 
-  --connect jdbc:mysql://172.31.54.174/rajeshk 
+  --connect jdbc:mysql://<server_ip>/rajeshk 
   --driver com.mysql.jdbc.Driver 
-  --username labuser 
-  --password simplilearn 
+  --username <db_user_name> 
+  --password <db_user_name_password>
   --table emp 
   -m 1
 
-# -m 1 represents that there is one mapper
-# by default there are 4 mappers used when no mapper is specified
-# this command creates file in the HDFS in the home directory for the user ~/emp/part-m-00000
-# the m represents that this is a mapper job
+
