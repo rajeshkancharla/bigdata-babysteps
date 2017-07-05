b99fdec9678abf64aef613fd5a112a0310e96d72
Rajesh Kancharla <rajeshkancharla@users.noreply.github.com>
2017-07-05 22:36:45 +0800

    Update 02_sqoop_commands.sh

--- 02_sqoop_commands.sh
+++ 02_sqoop_commands.sh
@@ -22,6 +22,7 @@ sqoop list-tables
     # by default there are 4 mappers used when no mapper is specified
     # this command creates file in the HDFS in the home directory for the user ~/emp/part-m-00000
     # the m represents that this is a mapper job, instead of =m --num-mappers can be used as well
+    # whenever a sqoop command is executed, a map reduce job kicks start and completes the activity
 sqoop import 
   --connect jdbc:mysql://<server_ip>/rajeshk 
   --driver com.mysql.jdbc.Driver 
@@ -60,3 +61,53 @@ sqoop import
   --split-by empno 
   --num-mappers 2
 
+
+# Import table from RDBMS to HDFS in particular directory
+  # By default the HDFS files are created in the home directory of the user
+  # If the directory already exists, the job fails with the message thet the folder already exists.
+  # Select a destination folder name that doesn't exist
+  # '--target-dir' specifies the destination directory name
+  # 'num-mappers' is same as 'm'
+sqoop import 
+  --connect jdbc:mysql://<server_ip>/rajeshk 
+  --driver com.mysql.jdbc.Driver 
+  --username <db_user_name> 
+  --password <db_user_name_password>
+  --table emp 
+  --split-by empno 
+  --target-dir hdata/sqoop_demo
+  --num-mappers 2
+
+
+# Incremental Import table from RDBMS to HDFS
+  # This helps in retrieving only rows newer than some previously imported set of rows
+  # Two modes of incremental import - append and lastmodified
+  # Use append when newer rows are continually being added with increasing rowid values
+  # '--check-column' specifies the column to be checked and '--last-value' specifies the last value available in HDFS
+  # lastmodified mode works if there is a timestamp that captures latest updates happened on a table
+sqoop import 
+  --connect jdbc:mysql://<server_ip>/rajeshk 
+  --driver com.mysql.jdbc.Driver 
+  --username <db_user_name> 
+  --password <db_user_name_password>
+  --table emp 
+  --split-by empno 
+  --target-dir hdata/sqoop_demo
+  --incremental append
+  --check-column empno
+  --last-value 7934
+  
+  
+# Export table from HDFS to RDBMS
+  # This helps in exporting the data from HDFS to RDBMS
+  # Data will be exported to a table that has no data in it
+  # Data will be exported from a directory having multiple mapper files
+  # Data can also be exported from a specific mapper file in a directory
+  # 
+sqoop export 
+  --connect jdbc:mysql://<server_ip>/rajeshk 
+  --driver com.mysql.jdbc.Driver 
+  --username <db_user_name> 
+  --password <db_user_name_password>
+  --table emp_export 
+  --export-dir emp
