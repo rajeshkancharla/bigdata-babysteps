a052a1a7d295dbf194d550d36b822d1b818f9f70
Rajesh Kancharla <rajeshkancharla@users.noreply.github.com>
2017-07-05 10:18:23 +0800

    SQL Statements - DDL and DML

---
+++ 01_sql_statements.sql
@@ -0,0 +1,78 @@
+-- Connect to My SQL Server
+-- In the following case, I have connected to a server where I have mysql installed and I have database login credentials
+
+mysql -h <server_name> -u <db_user_name> -p <db_user_name_password>
+
+CREATE DATABASE rajeshk;
+
+USE rajeshk;
+
+DROP TABLE IF EXISTS emp;
+
+CREATE TABLE emp (
+  empno decimal(4,0) NOT NULL PRIMARY KEY,
+  ename varchar(10) default NULL,
+  job varchar(9) default NULL,
+  mgr decimal(4,0) default NULL,
+  hiredate date default NULL,
+  sal decimal(7,2) default NULL,
+  comm decimal(7,2) default NULL,
+  deptno decimal(2,0) default NULL
+);
+
+DROP TABLE IF EXISTS dept;
+
+CREATE TABLE dept (
+  deptno decimal(2,0) default NULL,
+  dname varchar(14) default NULL,
+  loc varchar(13) default NULL
+);
+
+DROP TABLE IF EXISTS emp_export;
+
+CREATE TABLE emp_export (
+  empno decimal(4,0) NOT NULL PRIMARY KEY,
+  ename varchar(10) default NULL,
+  job varchar(9) default NULL,
+  mgr decimal(4,0) default NULL,
+  hiredate date default NULL,
+  sal decimal(7,2) default NULL,
+  comm decimal(7,2) default NULL,
+  deptno decimal(2,0) default NULL
+);
+
+DELETE FROM emp;
+
+COMMIT;
+
+INSERT INTO emp VALUES ('7369','SMITH','CLERK','7902','1980-12-17','800.00',NULL,'20');
+INSERT INTO emp VALUES ('7499','ALLEN','SALESMAN','7698','1981-02-20','1600.00','300.00','30');
+INSERT INTO emp VALUES ('7521','WARD','SALESMAN','7698','1981-02-22','1250.00','500.00','30');
+INSERT INTO emp VALUES ('7566','JONES','MANAGER','7839','1981-04-02','2975.00',NULL,'20');
+INSERT INTO emp VALUES ('7654','MARTIN','SALESMAN','7698','1981-09-28','1250.00','1400.00','30');
+INSERT INTO emp VALUES ('7698','BLAKE','MANAGER','7839','1981-05-01','2850.00',NULL,'30');
+INSERT INTO emp VALUES ('7782','CLARK','MANAGER','7839','1981-06-09','2450.00',NULL,'10');
+INSERT INTO emp VALUES ('7788','SCOTT','ANALYST','7566','1982-12-09','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7839','KING','PRESIDENT',NULL,'1981-11-17','5000.00',NULL,'10');
+INSERT INTO emp VALUES ('7844','TURNER','SALESMAN','7698','1981-09-08','1500.00','0.00','30');
+INSERT INTO emp VALUES ('7876','ADAMS','CLERK','7788','1983-01-12','1100.00',NULL,'20');
+INSERT INTO emp VALUES ('7900','JAMES','CLERK','7698','1981-12-03','950.00',NULL,'30');
+INSERT INTO emp VALUES ('7902','FORD','ANALYST','7566','1981-12-03','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7934','MILLER','CLERK','7782','1982-01-23','1300.00',NULL,'10');
+
+INSERT INTO emp VALUES ('8000','TESTER','CLERK','7782','1982-01-23','1300.00',NULL,'10');
+INSERT INTO emp VALUES ('8001','ADAMS','CLERK','7782','1982-01-23','1300.00',NULL,'10');
+INSERT INTO emp VALUES ('8002','MICHAEL','CLERK','7782','1982-01-23','1300.00',NULL,'10');
+
+COMMIT;
+
+DELETE FROM dept;
+
+COMMIT;
+
+INSERT INTO dept VALUES ('10','ACCOUNTING','NEW YORK');
+INSERT INTO dept VALUES ('20','RESEARCH','DALLAS');
+INSERT INTO dept VALUES ('30','SALES','CHICAGO');
+INSERT INTO dept VALUES ('40','OPERATIONS','BOSTON');
+
+COMMIT;
